d1d86d7b1b8fb8f56e3a233e1111d846d6f9c5ee
Amanrawat110 <108486431+Amanrawat110@users.noreply.github.com>
2024-09-13 23:54:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    <h1>e-Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Nav=()=>{
+    return(
+        <div>
+            <ul>
+                <li>home </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
